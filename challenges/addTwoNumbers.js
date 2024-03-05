fc6ee0d98b8729d867916a5ad36df1a2bbbb3102
Ryan Udelhoven <Ryan.Udelhoven@outlook.com>
2024-03-05 05:57:09 -0600

    addTwoNumbers

--- challenges/addTwoNumbers.js
+++ challenges/addTwoNumbers.js
@@ -1,4 +1,4 @@
-// Add the reverse of the digits in the array.
+// Add the reverse of the digits in the two arrays.
 // ['2', '3', '4'] = 432
 // ['6', '5', '4'] = 456
 // 432 + 456 = 888
@@ -11,10 +11,15 @@ let randomArray = () => {
   let length = Math.floor(Math.random() * 10) + 1;
   let output = [];
   for (i = 0; i < length; i++) {
-    output.push(randomInteger());
+    output.push(randomInteger().toString());
   }
   console.log(output);
   return output;
 };
 
-randomArray();
+let addTwoNumbers = (l1, l2) => {
+  return parseInt(l1.reverse().join('')) + parseInt(l2.reverse().join(''));
+};
+
+console.log('Example:', addTwoNumbers(['2', '3', '4'], ['6', '5', '4']));
+console.log(addTwoNumbers(randomArray(), randomArray()));
