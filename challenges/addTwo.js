6d8a00cbe8972f404aea6fe65dd7add210697332
Ryan Udelhoven <Ryan.Udelhoven@outlook.com>
2024-03-05 06:41:26 -0600

    addTwo

---
+++ challenges/addTwo.js
@@ -0,0 +1,16 @@
+// Given an array, find two of the numbers whose sum is the target number.
+// If the target is achievable, return the indices of both numbers.
+// If the target is unachievable, return false.
+
+const targetNumber = 9;
+const numberArray = [1, 2, 4, 7, 11, 14];
+
+function addTwo(target, array) {
+  for (number of array) {
+    const complimentIndex = array.indexOf(target - number);
+    if (complimentIndex !== -1) return [array.indexOf(number), complimentIndex];
+  }
+  return false;
+}
+
+console.log(addTwo(targetNumber, numberArray));
